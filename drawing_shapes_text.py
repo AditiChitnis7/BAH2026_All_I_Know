8e611840c8d164af7402eadafaeef8836d3d1b91
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-16 16:40:15 +0530

    Painting it partly

--- drawing_shapes_text.py
+++ drawing_shapes_text.py
@@ -8,3 +8,7 @@ cv.imshow('blank', blank)
 blank[:]= 0,255,0
 cv.imshow('Green', blank)
 cv.waitKey(0)
+#2. Painting a part of the image:
+blank[200:300, 300:400]= 0,0,255
+cv.imshow('Partly coloured', blank)
+cv.waitKey(0)
