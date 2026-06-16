14b5e06f5f81602439dfb5f52151c74ebb29ae7a
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-16 16:36:50 +0530

    Changing the colour of the image (Painting it)

--- drawing_shapes_text.py
+++ drawing_shapes_text.py
@@ -2,6 +2,9 @@ import cv2 as cv
 import numpy as np
 
 #creating blank image
-blank = np.zeros((500,500), dtype='uint8')
+blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('blank', blank)
+#1. Painting the image:
+blank[:]= 0,255,0
+cv.imshow('Green', blank)
 cv.waitKey(0)
