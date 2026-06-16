4c4c9083652bde32e5b618ef03dd7bab2fbaf9e0
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-16 16:47:02 +0530

    Making a rectangle (outline and filled)

--- drawing_shapes_text.py
+++ drawing_shapes_text.py
@@ -4,11 +4,20 @@ import numpy as np
 #creating blank image
 blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('blank', blank)
-#1. Painting the image:
-blank[:]= 0,255,0
-cv.imshow('Green', blank)
+##1. Painting the image:
+#blank[:]= 0,255,0
+#cv.imshow('Green', blank)
+#cv.waitKey(0)
+##2. Painting a part of the image:
+#blank[200:300, 300:400]= 0,0,255
+#cv.imshow('Partly coloured', blank)
+#cv.waitKey(0)
+
+#3. Drawing a rectangle
+cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=2) #Creating the outline of a RECTANGLE
+cv.imshow('Rectangle', blank)
 cv.waitKey(0)
-#2. Painting a part of the image:
-blank[200:300, 300:400]= 0,0,255
-cv.imshow('Partly coloured', blank)
+
+cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=cv.FILLED) #Creating a filled, green RECTANGLE (alternatively, thickness = -1 works too)
+cv.imshow('Rectangle', blank)
 cv.waitKey(0)
