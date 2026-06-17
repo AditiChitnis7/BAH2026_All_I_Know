a19c832bc21cd90b3c2aeaac8b77de184dafbcd9
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-17 17:31:41 +0530

    Detecting edges

--- greyscale.py
+++ greyscale.py
@@ -9,6 +9,11 @@ cv.imshow('greyscaled', gray)
 cv.waitKey(0)
 
 #2. Blurring
-blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blurred', blur)
+cv.waitKey(0)
+
+#3. Edge cascade: detecting edges in the image
+canny = cv.Canny(img, 125,175)
+cv.imshow('Canny Edges', canny)
 cv.waitKey(0)
