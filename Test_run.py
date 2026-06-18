5de2e879365cc21e6074a709da6afc5dad903289
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-18 11:40:40 +0530

    Flipping vetically

--- Test_run.py
+++ Test_run.py
@@ -3,20 +3,33 @@
 
 import cv2 as cv
 import numpy as np
+#
+#video= cv.VideoCapture(r"C:\Users\chitn\Videos\VID_20240807_205150.mp4")
+#def rescale(frame,scale=0.25):
+#    width = int(frame.shape[1]* scale)
+#    height = int(frame.shape[0]*scale)
+#    dimensions=(width, height)
+#    return cv.resize(frame, dimensions, interpolation = cv.INTER_LINEAR)
+#
+#while True:
+#    isTrue, frame = video.read()
+#    cv.imshow('Video', frame)
+#    resized = rescale(frame)
+#    cv.imshow('Resized Video', resized)
+#    if cv.waitKey(20) & 0xFF == ord('d'):
+#        break
+#video.release()
+#cv.destroyAllWindows()
+#
 
-video= cv.VideoCapture(r"C:\Users\chitn\Videos\VID_20240807_205150.mp4")
-def rescale(frame,scale=0.25):
-    width = int(frame.shape[1]* scale)
-    height = int(frame.shape[0]*scale)
-    dimensions=(width, height)
-    return cv.resize(frame, dimensions, interpolation = cv.INTER_LINEAR)
-
+capture = cv.VideoCapture(0)
 while True:
-    isTrue, frame = video.read()
-    cv.imshow('Video', frame)
-    resized = rescale(frame)
-    cv.imshow('Resized Video', resized)
+    isTrue, frames = capture.read()
+    
+    cv.imshow('Video', frames)
+    flipped_vertical = cv.flip(frames, 0)
+    cv.imshow('flipped', flipped_vertical)
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
-video.release()
-cv.destroyAllWindows()
+capture.release
+cv.destroyAllWindows 
