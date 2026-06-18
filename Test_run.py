3b97b1d4b0236c5a16a67d705a25aa7b08a09554
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-18 12:20:13 +0530

    Fixed last two lines

--- Test_run.py
+++ Test_run.py
@@ -31,5 +31,5 @@ while True:
     cv.imshow('flipped', flipped_vertical)
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
-capture.release
-cv.destroyAllWindows 
+capture.release()
+cv.destroyAllWindows()
