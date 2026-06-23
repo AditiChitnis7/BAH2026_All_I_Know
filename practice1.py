377ebdda274369cd199a720a3f6d85efcb4e6dc4
AditiChitnis7 <chitnisaditi75@gmail.com>
2026-06-23 15:36:17 +0530

    Attempt to reduce noise, no 2. (failed....badly)

--- practice1.py
+++ practice1.py
@@ -11,9 +11,9 @@ def rescale (img, scale=0.20):
 
 img= cv.imread(r"C:\Users\chitn\Downloads\HACKATHONDATA\OHRCDATA\browse\calibrated\20260103\ch2_ohr_ncp_20260103T1005176450_b_brw_d18.png")
 src =cv.normalize(img, None, 0, 255, cv.NORM_MINMAX, dtype = cv.CV_8U)
-blur= cv.GaussianBlur(src, (21,21),1)
+blur= cv.GaussianBlur(src, (21,21),3)
 greyscale = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
-clahe = cv.createCLAHE(clipLimit=1.0, tileGridSize=(6,6))
+clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(6,6))
 enhanced = clahe.apply(greyscale)
 canny = cv.Canny(blur, 100, 150) 
 kernel = np.ones((5,5), np.uint8)
